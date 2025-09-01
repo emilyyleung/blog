60239d25d2a5cd41f5a3ed91ef9bc15045855823
emilyyleung <emily_y_leung@outlook.com>
2025-09-01 22:40:03 +1000

    fix post title

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -37,7 +37,7 @@ export default function Feed({ pageSize = 5 }: { pageSize?: number }) {
           return (
             <article key={index}>
               <hr />
-              <h3>{post.title}</h3>
+              <h2>{post.title}</h2>
               <time>{new Date(post.date).toDateString()}</time>
               <div dangerouslySetInnerHTML={{ __html: post.html }} />
             </article>
